af68549bdee480f54f698ed67786054f16f81a7a
Ivan C. Sanches <sanches.ic.89@gmail.com>
2021-06-03 22:37:33 -0300

    🚀 get colors needed

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,30 @@
 export const theme = {
   colors: {
-    background: '#f9f9f9',
-    text: '#555555',
-    primary: '#6c63ff',
+    primary: '#803093',
+    secondary: '#ffffff',
+    input: {
+      label: '#404040',
+      border: '#d0e8e6',
+      active: {
+        border: '#a1d2ce',
+      },
+      disabled: {
+        border: '#989898',
+        background: '#f2f2f2',
+      },
+      error: {
+        border: '#ee90a2',
+        background: '#f8e7e7',
+      },
+    },
+    selectOption: {
+      hover: '#e1f0ef',
+    },
+    scrollbar: '#3f8794',
+    text: {
+      primary: '#000000',
+      secondary: '#ffffff',
+      error: '#ee4266',
+    },
   },
 }
