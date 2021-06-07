0e4cd499f1eb7595a5836f285585f5b1c5439431
Ivan C. Sanches <sanches.ic.89@gmail.com>
2021-06-07 13:48:23 -0300

    🛠 remove stories

--- src/components/Checkbox/Checkbox.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import {storiesOf} from '@storybook/react'
-
-import {Checkbox} from './Checkbox'
-
-storiesOf('Checkbox', module).add('default', () => {
-  return <Checkbox>Hello Checkbox!!</Checkbox>
-})
--- src/components/Select/Select.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import {storiesOf} from '@storybook/react'
-
-import {Select} from './Select'
-
-storiesOf('Select', module).add('default', () => {
-  return <Select>Hello Select!!</Select>
-})
